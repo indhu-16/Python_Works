e1671b78dc6e29f704e05997fba09c1a80baad1e
Indhu-16 <indhusekar1609@gmail.com>
2026-09-11 12:55:57 +0530

    Initial commit

---
+++ python/python task/task.py
@@ -0,0 +1,27 @@
+'''A. Python is an indent based programming language
+The following program throws an indentation error. Correct it and make sure it prints p'''
+
+teams = ['Data', 'AI', 'DevOps']
+for t in teams:
+    print('Hello', t, 'Team from Inceptez Technologies')
+    print('Keep Learning and Exploring!')
+
+'''Use Case 1:
+Add single-line and multi-line comments to describe what the below code does for Inceptez Technologies’ training tracker.'''
+
+# number of students declared as a variable
+students = 100
+#number of trainers declared in variable called trainers
+trainers = 2
+#calculate total students and trainers declared as a variable called total then print the value
+total = students + trainers
+print(total)
+
+
+#C. Playing with Quotes
+#This is Inceptez's "Python" class for Data Engineers & AI Engineers
+
+
+'''Welcome to Inceptez Technologies!
+Python Training: Basics
+Enjoy your learning journey
